eb16db99e44adf40c5bfee487b936597c4116cca
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 00:43:02 +0700

    the renderCharacter() function

--- index.js
+++ index.js
@@ -6,24 +6,64 @@ Challenge:
 2. Do the same for Orc card. 
 */
 
-document.getElementById('hero').innerHTML = `
-  <div class="character-card">
-    <h4 class="name"> Wizard </h4>
-    <img class="avatar" src="images/wizard.png" />
-    <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container">
-      <div class="dice"> 6 </div>
-    </div>
-  </div>
-`;
+// document.getElementById('hero').innerHTML = `
+//   <div class="character-card">
+//     <h4 class="name"> Wizard </h4>
+//     <img class="avatar" src="images/wizard.png" />
+//     <p class="health">health: <b> 60 </b></p>
+//     <div class="dice-container">
+//       <div class="dice"> 6 </div>
+//     </div>
+//   </div>
+// `;
 
-document.getElementById('monster').innerHTML = `
+// document.getElementById('monster').innerHTML = `
+//   <div class="character-card">
+//     <h4 class="name"> Orc </h4>
+//     <img class="avatar" src="images/orc.png" />
+//     <p class="health">health: <b> 10 </b></p>
+//     <div class="dice-container">
+//       <div class="dice"> 4 </div>
+//     </div>
+//   </div>
+// `;
+
+/*
+CHALLENGE
+1. Strip out the hero and monster data (element id, name, avatar, 
+health and dice score) and store them in variables
+
+2. Write a renderCharacter() function that accepts the 5 new variables 
+as paramaters and renders out a character with this data
+
+3. Call renderCharacter() twice. Once with the hero variables and 
+once with the monster variables to that both are rendered
+*/
+
+const heroElementId = 'hero';
+const heroName = 'Wizard';
+const heroAvatar = 'images/wizard.png';
+const heroHealth = '60';
+const heroDiceRoll = 6;
+
+const monsterElementId = 'monster';
+const monsterName = 'Orc';
+const monsterAvatar = 'images/orc.png';
+const monsterHealth = '10';
+const monsterDiceRoll = 4;
+
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+  document.getElementById(elementId).innerHTML = `
   <div class="character-card">
-    <h4 class="name"> Orc </h4>
-    <img class="avatar" src="images/orc.png" />
-    <p class="health">health: <b> 10 </b></p>
+    <h4 class="name"> ${name} </h4>
+    <img class="avatar" src="${avatar}" />
+    <p class="health">health: <b> ${health} </b></p>
     <div class="dice-container">
-      <div class="dice"> 4 </div>
+      <div class="dice"> ${diceRoll} </div>
     </div>
   </div>
 `;
+}
+
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll);
+renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll);
