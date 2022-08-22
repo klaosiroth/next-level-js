937039d1926acb65e6a067b5fbad92d22fd73f2f
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 01:41:01 +0700

    simplify constructor function

--- index.js
+++ index.js
@@ -563,18 +563,132 @@ Challenge
    function.
 */
 
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
+
+// function getDiceHtml(diceCount) {
+//   return getDiceRollArray(diceCount)
+//     .map(function (num) {
+//       return `<div class="dice">${num}</div>`;
+//     })
+//     .join('');
+// }
+
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceCount: 1,
+// };
+
+// function Character(data) {
+//   this.elementId = data.elementId;
+//   this.name = data.name;
+//   this.avatar = data.avatar;
+//   this.health = data.health;
+//   this.diceCount = data.diceCount;
+
+//   this.getCharacterHtml = function () {
+//     const { elementId, name, avatar, health, diceCount } = this;
+//     const diceHtml = getDiceHtml(diceCount);
+
+//     document.getElementById(elementId).innerHTML = `
+//       <div class="character-card">
+//         <h4 class="name"> ${name} </h4>
+//         <img class="avatar" src="${avatar}" />
+//         <div class="health">health: <b> ${health} </b></div>
+//         <div class="dice-container">
+//           ${diceHtml}
+//         </div>
+//       </div>`;
+//   };
+// }
+
+// const wizard = new Character(hero);
+// const orc = new Character(monster);
+// wizard.getCharacterHtml();
+// orc.getCharacterHtml();
+
+/*CHALLENGE
+-Use what you learned in the previous scrim to
+remove the following 5 lines of code
+*/
+
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
+
+// function getDiceHtml(diceCount) {
+//   return getDiceRollArray(diceCount)
+//     .map(function (num) {
+//       return `<div class="dice">${num}</div>`;
+//     })
+//     .join('');
+// }
+
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceCount: 1,
+// };
+
+// function Character(data) {
+//   Object.assign(this, data);
+
+//   this.getCharacterHtml = function () {
+//     const { elementId, name, avatar, health, diceCount } = this;
+//     let diceHtml = getDiceHtml(diceCount);
+
+//     document.getElementById(elementId).innerHTML = `
+//       <div class="character-card">
+//         <h4 class="name"> ${name} </h4>
+//         <img class="avatar" src="${avatar}" />
+//         <div class="health">health: <b> ${health} </b></div>
+//         <div class="dice-container">
+//           ${diceHtml}
+//         </div>
+//       </div>`;
+//   };
+// }
+
+// const wizard = new Character(hero);
+// wizard.getCharacterHtml();
+
+// const orc = new Character(monster);
+// orc.getCharacterHtml();
+
+// CHALLENGE
+// 1. Add this function as a method of our character constructor
+// 2. Make a small change to getCharacterHtml to make the app work again
+
 function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(function () {
     return Math.floor(Math.random() * 6) + 1;
   });
-}
-
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map(function (num) {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join('');
 }
 
 const hero = {
@@ -594,22 +708,26 @@ const monster = {
 };
 
 function Character(data) {
-  this.elementId = data.elementId;
-  this.name = data.name;
-  this.avatar = data.avatar;
-  this.health = data.health;
-  this.diceCount = data.diceCount;
+  Object.assign(this, data);
+
+  this.getDiceHtml = function (diceCount) {
+    return getDiceRollArray(diceCount)
+      .map(function (num) {
+        return `<div class="dice">${num}</div>`;
+      })
+      .join('');
+  };
 
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
-    const diceHtml = getDiceHtml(diceCount);
+    let diceHtml = this.getDiceHtml(diceCount);
 
     document.getElementById(elementId).innerHTML = `
       <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}" />
         <div class="health">health: <b> ${health} </b></div>
-        <div class="dice-container">    
+        <div class="dice-container">
           ${diceHtml}
         </div>
       </div>`;
@@ -617,6 +735,7 @@ function Character(data) {
 }
 
 const wizard = new Character(hero);
-const orc = new Character(monster);
 wizard.getCharacterHtml();
+
+const orc = new Character(monster);
 orc.getCharacterHtml();
