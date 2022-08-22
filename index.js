d91159438b1b68b707d95ed293c8298547eb0a9b
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 00:57:44 +0700

    the problem with for loops

--- index.js
+++ index.js
@@ -158,12 +158,58 @@ the diceCount property of the objects.
 5. Swap out the diceRoll variable for diceHtml in the template
 */
 
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceRoll: 6,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceRoll: 4,
+//   diceCount: 1,
+// };
+
+// function renderCharacter(data) {
+//   const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+//   let diceHtml = '';
+
+//   for (let i = 0; i < diceCount; i++) {
+//     diceHtml += `<div class="dice">6</div>`;
+//   }
+
+//   document.getElementById(elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${name} </h4>
+//       <img class="avatar" src="${avatar}" />
+//       <div class="health">health: <b> ${health} </b></div>
+//       <div class="dice-container">
+//         ${diceHtml}
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+// CHALLENGE
+// Update this for loop so it uses a value from the
+// new diceRoll array to render out the dice so the
+// wizard's dice have values of 3, 1 and 4, and the
+// orc's single dice has a value of 2.
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
   diceCount: 3,
 };
 
@@ -172,7 +218,7 @@ const monster = {
   name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceRoll: 4,
+  diceRoll: [2],
   diceCount: 1,
 };
 
@@ -180,8 +226,8 @@ function renderCharacter(data) {
   const { elementId, name, avatar, health, diceRoll, diceCount } = data;
   let diceHtml = '';
 
-  for (let i = 0; i < diceCount; i++) {
-    diceHtml += `<div class="dice">6</div>`;
+  for (let i = 0; i < diceRoll.length; i++) {
+    diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
   }
 
   document.getElementById(elementId).innerHTML = `
