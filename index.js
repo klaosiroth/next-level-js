11f470f30eb9244f867176c3e78b962bfab944dd
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 00:49:38 +0700

    object destructuring in renderCharacter()

--- index.js
+++ index.js
@@ -79,6 +79,41 @@ reducing the number of arguments to pass in from five to one.
 4. Update the function call.
 */
 
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceRoll: 6,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceRoll: 4,
+// };
+
+// function renderCharacter(data) {
+//   document.getElementById(data.elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${data.name} </h4>
+//       <img class="avatar" src="${data.avatar}" />
+//       <div class="health">health: <b> ${data.health} </b></div>
+//       <div class="dice-container">
+//         <div class="dice"> ${data.diceRoll} </div>
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+// CHALLENGE
+// 1. Deconstruct the data object
+// 2. Update the template string as necessary
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
@@ -96,13 +131,14 @@ const monster = {
 };
 
 function renderCharacter(data) {
-  document.getElementById(data.elementId).innerHTML = `
+  const { elementId, name, avatar, health, diceRoll } = data;
+  document.getElementById(elementId).innerHTML = `
     <div class="character-card">
-      <h4 class="name"> ${data.name} </h4>
-      <img class="avatar" src="${data.avatar}" />
-      <div class="health">health: <b> ${data.health} </b></div>
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}" />
+      <div class="health">health: <b> ${health} </b></div>
       <div class="dice-container">
-        <div class="dice"> ${data.diceRoll} </div>
+        <div class="dice"> ${diceRoll} </div>
       </div>
     </div>`;
 }
