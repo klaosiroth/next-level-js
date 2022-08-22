9daf43283557c1fce721d4f1b6977fb2c495f8a8
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 00:56:06 +0700

    render multiple dice

--- index.js
+++ index.js
@@ -114,12 +114,57 @@ reducing the number of arguments to pass in from five to one.
 // 1. Deconstruct the data object
 // 2. Update the template string as necessary
 
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceRoll: 6,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceRoll: 4,
+// };
+
+// function renderCharacter(data) {
+//   const { elementId, name, avatar, health, diceRoll } = data;
+//   document.getElementById(elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${name} </h4>
+//       <img class="avatar" src="${avatar}" />
+//       <div class="health">health: <b> ${health} </b></div>
+//       <div class="dice-container">
+//         <div class="dice"> ${diceRoll} </div>
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+/*
+CHALLENGE
+1. declare a let called diceHtml and initialize it with an empty 
+string. 
+2. Use a for loop to update diceHtml so that it contains the 
+HTML for our dice. The number of dice needed is specificed in 
+the diceCount property of the objects.
+3. Each dice should have the following HTML: <div class="dice">6</div>
+4. For now, each dice will display 6
+5. Swap out the diceRoll variable for diceHtml in the template
+*/
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
   diceRoll: 6,
+  diceCount: 3,
 };
 
 const monster = {
@@ -128,17 +173,24 @@ const monster = {
   avatar: 'images/orc.png',
   health: 10,
   diceRoll: 4,
+  diceCount: 1,
 };
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  let diceHtml = '';
+
+  for (let i = 0; i < diceCount; i++) {
+    diceHtml += `<div class="dice">6</div>`;
+  }
+
   document.getElementById(elementId).innerHTML = `
     <div class="character-card">
       <h4 class="name"> ${name} </h4>
       <img class="avatar" src="${avatar}" />
       <div class="health">health: <b> ${health} </b></div>
       <div class="dice-container">
-        <div class="dice"> ${diceRoll} </div>
+        ${diceHtml}
       </div>
     </div>`;
 }
