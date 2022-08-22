65a63e53232ce0eac5d1997746887465c04fcdf9
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 01:03:47 +0700

    swap out for loop for map

--- index.js
+++ index.js
@@ -204,6 +204,54 @@ the diceCount property of the objects.
 // wizard's dice have values of 3, 1 and 4, and the
 // orc's single dice has a value of 2.
 
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceRoll: [3, 1, 4],
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceRoll: [2],
+//   diceCount: 1,
+// };
+
+// function renderCharacter(data) {
+//   const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+//   let diceHtml = '';
+
+//   for (let i = 0; i < diceRoll.length; i++) {
+//     diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
+//   }
+
+//   document.getElementById(elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${name} </h4>
+//       <img class="avatar" src="${avatar}" />
+//       <div class="health">health: <b> ${health} </b></div>
+//       <div class="dice-container">
+//         ${diceHtml}
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+/*
+CHALLENGE
+1. Instead of the for loop, map over the diceRoll array
+and save the new array to diceHTML.
+2. Remember to deal with the commas between dice.
+3. What keyword should be used to declare diceHTML? 
+*/
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
@@ -224,11 +272,11 @@ const monster = {
 
 function renderCharacter(data) {
   const { elementId, name, avatar, health, diceRoll, diceCount } = data;
-  let diceHtml = '';
-
-  for (let i = 0; i < diceRoll.length; i++) {
-    diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
-  }
+  const diceHtml = diceRoll
+    .map((num) => {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join('');
 
   document.getElementById(elementId).innerHTML = `
     <div class="character-card">
