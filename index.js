24647bf25ac93b5b449af0f54f12ffde2f9e0840
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 01:43:39 +0700

    problem in the constructor function

--- index.js
+++ index.js
@@ -685,6 +685,68 @@ remove the following 5 lines of code
 // 1. Add this function as a method of our character constructor
 // 2. Make a small change to getCharacterHtml to make the app work again
 
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
+
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceCount: 1,
+// };
+
+// function Character(data) {
+//   Object.assign(this, data);
+
+//   this.getDiceHtml = function (diceCount) {
+//     return getDiceRollArray(diceCount)
+//       .map(function (num) {
+//         return `<div class="dice">${num}</div>`;
+//       })
+//       .join('');
+//   };
+
+//   this.getCharacterHtml = function () {
+//     const { elementId, name, avatar, health, diceCount } = this;
+//     let diceHtml = this.getDiceHtml(diceCount);
+
+//     document.getElementById(elementId).innerHTML = `
+//       <div class="character-card">
+//         <h4 class="name"> ${name} </h4>
+//         <img class="avatar" src="${avatar}" />
+//         <div class="health">health: <b> ${health} </b></div>
+//         <div class="dice-container">
+//           ${diceHtml}
+//         </div>
+//       </div>`;
+//   };
+// }
+
+// const wizard = new Character(hero);
+// wizard.getCharacterHtml();
+
+// const orc = new Character(monster);
+// orc.getCharacterHtml();
+
+/*
+CHALLENGE
+- instead of setting the innerHTML from right here in the contructor,
+make it so that we just return that literal string of HTML
+- This will break the app. Don't worry!
+*/
+
 function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(function () {
     return Math.floor(Math.random() * 6) + 1;
@@ -722,7 +784,7 @@ function Character(data) {
     const { elementId, name, avatar, health, diceCount } = this;
     let diceHtml = this.getDiceHtml(diceCount);
 
-    document.getElementById(elementId).innerHTML = `
+    return `
       <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}" />
