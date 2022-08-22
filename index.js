0383c51cd9247c324017975346375ee88973fd88
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 01:37:18 +0700

    use a constructor function

--- index.js
+++ index.js
@@ -424,18 +424,149 @@ with 0s as its initial state. The new array should be diceRoll length.
 and return a random number from 1-6 in each element of the array.
 3. Make sure you delete anything from that function that we no longer need.
 */
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
+
+// function getDiceRollArray(diceCount) {
+//   let newDiceRolls = [];
+//   for (let i = 0; i < diceCount; i++) {
+//     newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+//   }
+//   return newDiceRolls;
+// }
+
+// function getDiceHtml(diceCount) {
+//   return getDiceRollArray(diceCount)
+//     .map(function (num) {
+//       return `<div class="dice">${num}</div>`;
+//     })
+//     .join('');
+// }
+
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceCount: 1,
+// };
+// function renderCharacter(data) {
+//   const { elementId, name, avatar, health, diceCount } = data;
+//   const diceHtml = getDiceHtml(diceCount);
+
+//   document.getElementById(elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${name} </h4>
+//       <img class="avatar" src="${avatar}" />
+//       <div class="health">health: <b> ${health} </b></div>
+//       <div class="dice-container">
+//         ${diceHtml}
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+/*
+Challenge 
+1. Create a new constructor function called Character which
+   takes our data as a paramenter.
+2. Set up "this" for each of the 5 properties in our objects
+   (eg: this.health = data.health).
+*/
+
+// function getDiceRollArray(diceCount) {
+//   return new Array(diceCount).fill(0).map(function () {
+//     return Math.floor(Math.random() * 6) + 1;
+//   });
+// }
+
+// function getDiceRollArray(diceCount) {
+//   let newDiceRolls = [];
+//   for (let i = 0; i < diceCount; i++) {
+//     newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+//   }
+//   return newDiceRolls;
+// }
+
+// function getDiceHtml(diceCount) {
+//   return getDiceRollArray(diceCount)
+//     .map(function (num) {
+//       return `<div class="dice">${num}</div>`;
+//     })
+//     .join('');
+// }
+
+// const hero = {
+//   elementId: 'hero',
+//   name: 'Wizard',
+//   avatar: 'images/wizard.png',
+//   health: 60,
+//   diceCount: 3,
+// };
+
+// const monster = {
+//   elementId: 'monster',
+//   name: 'Orc',
+//   avatar: 'images/orc.png',
+//   health: 10,
+//   diceCount: 1,
+// };
+
+// function Character(data) {
+//   this.elementId = data.elementId;
+//   this.name = data.name;
+//   this.avatar = data.avatar;
+//   this.health = data.health;
+//   this.diceCount = data.diceCount;
+// }
+
+// function renderCharacter(data) {
+//   const { elementId, name, avatar, health, diceCount } = data;
+//   const diceHtml = getDiceHtml(diceCount);
+
+//   document.getElementById(elementId).innerHTML = `
+//     <div class="character-card">
+//       <h4 class="name"> ${name} </h4>
+//       <img class="avatar" src="${avatar}" />
+//       <div class="health">health: <b> ${health} </b></div>
+//       <div class="dice-container">
+//         ${diceHtml}
+//       </div>
+//     </div>`;
+// }
+
+// renderCharacter(hero);
+// renderCharacter(monster);
+
+/*
+Challenge
+1. Create a method called getCharacterHtml that performs the 
+   same tasks as our current renderCharacter function.
+2. Create two new instances of Character. One for a hero, 
+   called "wizard", and one for a monster, called "orc". 
+   Render both of them on the page.
+3. Delete both the old renderCharacter function and the two 
+   lines of code at the bottom of the page which invoke that 
+   function.
+*/
+
 function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(function () {
     return Math.floor(Math.random() * 6) + 1;
   });
-}
-
-function getDiceRollArray(diceCount) {
-  let newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
 }
 
 function getDiceHtml(diceCount) {
@@ -461,20 +592,31 @@ const monster = {
   health: 10,
   diceCount: 1,
 };
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
-  const diceHtml = getDiceHtml(diceCount);
 
-  document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-      <h4 class="name"> ${name} </h4>
-      <img class="avatar" src="${avatar}" />
-      <div class="health">health: <b> ${health} </b></div>
-      <div class="dice-container">
-        ${diceHtml}
-      </div>
-    </div>`;
+function Character(data) {
+  this.elementId = data.elementId;
+  this.name = data.name;
+  this.avatar = data.avatar;
+  this.health = data.health;
+  this.diceCount = data.diceCount;
+
+  this.getCharacterHtml = function () {
+    const { elementId, name, avatar, health, diceCount } = this;
+    const diceHtml = getDiceHtml(diceCount);
+
+    document.getElementById(elementId).innerHTML = `
+      <div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <div class="health">health: <b> ${health} </b></div>
+        <div class="dice-container">    
+          ${diceHtml}
+        </div>
+      </div>`;
+  };
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+const wizard = new Character(hero);
+const orc = new Character(monster);
+wizard.getCharacterHtml();
+orc.getCharacterHtml();
